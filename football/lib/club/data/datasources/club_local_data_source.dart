5560f217f81b746ac6acaa6a8f36a93a2a98fea1
Sadra <31988724+xsadra@users.noreply.github.com>
2021-04-04 22:49:03 +0200

    Save and Read Data to/from local

--- football/lib/club/data/datasources/club_local_data_source.dart
+++ football/lib/club/data/datasources/club_local_data_source.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:football/club/data/models/club_model.dart';
+import 'package:football/core/error/exceptions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const CACHED_CLUBS = 'CACHED_CLUBS';
 
 abstract class ClubLocalDataSource {
   /// Gets the cached list of [ClubModel] which was gotten the last time.
@@ -8,4 +15,30 @@ abstract class ClubLocalDataSource {
   Future<List<ClubModel>> getLastData();
 
   Future<void> cacheClubs(List<ClubModel> clubsToCache);
+}
+
+class ClubLocalDataSourceImpl implements ClubLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  ClubLocalDataSourceImpl({
+    @required this.sharedPreferences,
+  });
+
+  @override
+  Future<void> cacheClubs(List<ClubModel> clubsToCache) {
+    return sharedPreferences.setString(
+      CACHED_CLUBS,
+      jsonEncode(
+          clubsToCache), // if doesn't work create listToJson in ClubModel
+    );
+  }
+
+  @override
+  Future<List<ClubModel>> getLastData() {
+    final jsonString = sharedPreferences.getString(CACHED_CLUBS);
+    if (jsonString == null) {
+      throw CacheException();
+    }
+    return Future.value(ClubModel.fromJsonList(jsonString));
+  }
 }
